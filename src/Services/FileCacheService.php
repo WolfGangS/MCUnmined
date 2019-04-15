806fe93798af9468754b794a070b1d7a6616a8b5
WolfGang <flamin2k8@gmail.com>
2019-04-15 22:49:59 +0100

    data

--- src/Services/FileCacheService.php
+++ src/Services/FileCacheService.php
@@ -6,6 +6,9 @@ use WLF_IO\MCUnmined\Interfaces\CacheInterface;
 
 class FileCacheService implements CacheInterface
 {
+    const VALUE = "value";
+    const TTL = "ttl";
+
     private $file = APP_ROOT . "/cache/filecache.json";
     private $_cache = [];
 
@@ -18,31 +21,51 @@ class FileCacheService implements CacheInterface
 
     public function get(string $key)
     {
+        return $this->getCacheValue($key)["value"];
+    }
+
+    private function getCacheValue(string $key)
+    {
         return $this->getCache()[$key] ?? null;
     }
 
     public function set(string $key, string $val, int $ttl = 0): bool
     {
-        $this->_cache[$key] = $val;
+        $this->getCache();
+        $this->_cache[$key]["value"] = $val;
+        $this->_cache[$key][self::TTL] = $ttl === 0 ? 0 : time() + $ttl;
         $this->saveCache();
         return true;
     }
 
-    private function saveCache(){
+    private function saveCache()
+    {
         $dir = dirname($this->file);
-        if(!file_exists($dir)){
+        if (!file_exists($dir)) {
             mkdir($dir);
+        }
+        $data = [];
+        $time = time();
+        foreach ($this->getCache() as $key => $value) {
+            if ($value[self::TTL] > $time) {
+                $data[$key] = $value;
+            }
         }
         file_put_contents($this->file, json_encode($this->_cache, JSON_PRETTY_PRINT));
     }
 
     private function getCache()
     {
-        if(empty($this->_cache)){
-            if(file_exists($this->file)) {
+        if (empty($this->_cache)) {
+            if (file_exists($this->file)) {
                 $cache = json_decode(file_get_contents($this->file), true);
                 if (is_array($cache)) {
-                    $this->_cache = $cache;
+                    $time = time();
+                    foreach ($cache as $key => $value) {
+                        if ($value[self::TTL] > $time) {
+                            $this->_cache[$key] = $value;
+                        }
+                    }
                 }
             }
         }
