3b33563d3ff3e87eefb2bd7e3f43fc58575412d5
WolfGang <flamin2k8@gmail.com>
2019-04-15 21:26:11 +0100

    data

--- src/Controllers/PlayerController.php
+++ src/Controllers/PlayerController.php
@@ -4,9 +4,10 @@ namespace WLF_IO\MCUnmined\Controllers;
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use WLF_IO\MCUnmined\Controllers\Base\BaseController;
 use WLF_IO\MCUnmined\Services\PlayerService;
 
-class PlayerController
+class PlayerController extends BaseController
 {
     private $service;
 
@@ -16,7 +17,6 @@ class PlayerController
     }
 
     public function listRequest(Request $request, Response $response) {
-        $response->getBody()->write(json_encode($this->service->getPlayerData()));
-        return $response;
+        return $this->success($this->service->getPlayerData(),$response);
     }
 }
